b6a2d880b81a3afacdd5d6fee5f409194f7bbe08
root <sersavvov@yandex.ru>
2020-07-09 11:24:47 +0300

    Refactor Perecrestok parser

---
+++ receipt_parser/parsers/perecrestok.py
@@ -0,0 +1,131 @@
+"""Parse info about Perecrestok supremarket: `https://www.perekrestok.ru`."""
+from time import sleep
+from typing import Dict, Optional, NamedTuple, Set, NoReturn
+import requests as req
+from selenium import webdriver  # type: ignore
+from bs4 import BeautifulSoup  # type: ignore
+import pandas as pd  # type: ignore
+
+Category = NamedTuple('Category', [('name', str), ('url', str)])
+
+
+class Perecrestok:
+    """Allow to parse all data about products from the Perecrestok."""
+
+    def __init__(self):
+        self.main_url = "https://www.perekrestok.ru"
+        self.url_catalog = "https://www.perekrestok.ru/catalog"
+        self.columns = [
+            "Название",
+            "Категория",
+            "Производитель",
+            "Торговая марка",
+            "Вес",
+            "Жирность",
+        ]
+        self.result = pd.DataFrame(columns=self.columns)
+
+    @staticmethod
+    def __get_html(url: str) -> str:
+        """Scroll down a HTML page and return the HTML-code."""
+
+        driver = webdriver.Chrome()
+        driver.get(url)
+        for i in range(0, 50000, 1080):
+            driver.execute_script(f"window.scrollTo({i}, {i+1080})")
+            sleep(3)
+        page = driver.page_source
+        driver.close()
+        return page
+
+    @staticmethod
+    def __get_product_name(soup: BeautifulSoup) -> str:
+        """Return the porduct name."""
+
+        name = soup.find(class_="js-product__title xf-product-card__title")
+        if name:
+            name = name.text.split("\n")[0]
+        return name
+
+    @staticmethod
+    def __log_error(function_name: str) -> NoReturn:
+        """Raise error if status code not equal 200."""
+
+        raise ValueError(f"Проблема с подключением к сети в функции {function_name}.")
+
+    def get_catalog(self) -> Set[Category]:
+        """Return set of namedtuples about all categories in the catalog."""
+
+        result: Set[Category] = set()
+        resp = req.get(self.url_catalog)
+        if resp.status_code != 200:
+            self.__log_error(self.get_catalog.__name__)
+        soup = BeautifulSoup(resp.text, "lxml")
+        for cat in soup.find_all(class_="xf-catalog-categories__item"):
+            href = cat.find(class_="xf-catalog-categories__link").get("href")
+            name = cat.text.strip()
+            result.add(Category(name, self.main_url + href))
+
+        return result
+
+    def __parse_good(self, url: str) -> Dict[str, Optional[str]]:
+        """Parse information about the product."""
+
+        product: Dict[str, Optional[str]] = dict()
+        _ = [product.setdefault(key, None) for key in self.columns]
+        resp = req.get(url)
+        if resp.status_code != 200:
+            self.__log_error(self.__parse_good.__name__)
+        soup = BeautifulSoup(resp.text, "lxml")
+        product["Название"] = self.__get_product_name(soup)
+        table = soup.find(
+            "table", attrs={"class": "xf-product-info__table xf-product-table"}
+        )
+        if table:
+            rows = table.find_all("tr")
+            for row in rows:
+                key = row.find_all(class_="xf-product-table__col-header")[
+                    0
+                ].text.strip()
+                value = row.find_all("td")[0].text.strip()
+                if key == "Объём":
+                    key = "Вес"
+                if key in self.columns:
+                    product[key] = value
+        return product
+
+    def __parse_category(self, category: Category) -> pd.DataFrame:
+        """Parse all products in the categoty."""
+
+        print(f"Start parsing {category.name}.")
+        page = self.__get_html(category.url)
+        soup = BeautifulSoup(page, "lxml")
+        goods = soup.find_all(class_="js-catalog-product _additionals xf-catalog__item")
+        for good in goods:
+            url = good.find(class_="xf-product-picture__link js-product__image").get(
+                "href"
+            )
+            url = self.main_url + url
+            product = self.__parse_good(url)
+            product["Категория"] = category.name
+            self.result = pd.concat(
+                [self.result, pd.DataFrame.from_dict(product, orient="index").T]
+            )
+
+        self.result = self.result.dropna(subset=["Название"]).drop_duplicates(
+            subset=["Название"]
+        )
+        return self.result
+
+    def parse(self) -> pd.DataFrame:
+        """Parse all products descriptions from `https://www.perekrestok.ru`."""
+
+        for category in self.get_catalog():
+            self.__parse_category(category)
+        return self.result
+
+
+if __name__ == "__main__":
+    parser = Perecrestok()
+    data = parser.parse()
+    data.to_csv('perecrestok_goods.csv')
---
+++ receipt_parser/parsers/tinkoff.py
@@ -0,0 +1,91 @@
+"""Use Tinkoff(`https://receiptnlp.tinkoff.ru/`) to parse product goods."""
+import sys
+from time import sleep
+from typing import List
+from multiprocessing import Pool
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class Tinkoff:
+    """Use Tinkoff server to parse product goods. Use Chrome browser."""
+
+    def __init__(self):
+        self.url = 'https://receiptnlp.tinkoff.ru/'
+
+    def get_session(self) -> None:
+        """Open browser."""
+
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.url)
+
+    def fil_fields(self, name: str) -> None:
+        """Delete last text in the field and add new one."""
+
+        window = self.driver.find_element_by_class_name('_3gY2s')
+        window.send_keys(Keys.CONTROL + "a")
+        window.send_keys(Keys.DELETE)
+        window.send_keys(name)
+        button = self.driver.find_element_by_xpath("//button[@type='submit']")
+        button.click()
+        sleep(0.5)
+
+    def parse_table(self) -> dict:
+        """Parse information on the check."""
+
+        table = self.driver.find_element_by_class_name("Mnf-LQLHUyIQdT1VvKhCE")
+        result = {}
+        for row in table.text.split('\n'):
+            result[row.split()[0]] = ' '.join(row.split()[1:])
+        return result
+
+    def parse_data(self, products: List[str]) -> List[dict]:
+        """Start parsing data."""
+
+        self.get_session()
+        result = []
+        for name in products:
+            self.fil_fields(name)
+            result.append(self.parse_table())
+        self.driver.close()
+        return result
+
+
+def get_batches(products: List[str], processes_count: int) -> List[List[str]]:
+    """Split List to List[List] for using the multiprocessing."""
+
+    result = []
+    bath_size = round(len(products) / processes_count)
+    for i in range(0, len(products), bath_size):
+        result.append(products[i: i + bath_size])
+    return result
+
+
+def prepare_data_to_parse(path: str, processes_count: int) -> List[List[str]]:
+    """Read data and prepare it to use in a multiprocessing."""
+
+    df = pd.read_csv(path)
+    target_column = 'Название'
+    if target_column not in df.columns:
+        raise KeyError('Dataset must have a column with name "Название"')
+    df = df['Название'].apply(lambda x: [x]).values.tolist()
+    return get_batches(df, processes_count)
+
+
+def transform_and_save(data: List[List[dict]], path_to_save: str) -> None:
+    """Expand list if lists and save as csv file."""
+
+    data = [item for elem in data for item in elem]  # List[List[dict]] -> List[dict]
+    pd.DataFrame.from_dict(data).to_csv(path_to_save, index=False)
+
+
+if __name__ == '__main__':
+    _, path_to_db, path_to_save, processes_count = sys.argv
+    processes_count = int(processes_count)
+    data = prepare_data_to_parse(path_to_db, processes_count)
+    tinkoff = Tinkoff()
+    pool = Pool(processes_count)
+    result = pool.map(tinkoff.parse_data, data)
+    transform_and_save(result, path_to_save)
+    pool.close()
