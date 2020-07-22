044205feceb3b97dc88e75b0900bf0081e759095
slgero <sersavvov@yandex.ru>
2020-07-22 17:52:58 +0300

    Add setup file

--- receipt_parser/__init__.py
+++ receipt_parser/__init__.py
@@ -1,3 +1,6 @@
+__version__ = '0.0.1'
+__license__ = 'MIT'
+
 from .receipt_parser import RuleBased
 from .finder import Finder
 from .normalizer import Normalizer
---
+++ setup.py
@@ -0,0 +1,52 @@
+import os.path
+import re
+import setuptools
+
+
+def get_package_variable(name, rel_path="receipt_parser/__init__.py"):
+    path = os.path.join(os.path.abspath(os.path.dirname(name)), rel_path)
+
+    pattern = re.compile(r'^{}.*?([\'"])(?P<value>.+)\1.*$'.format(re.escape(name)))
+
+    with open(path, "r", encoding="UTF-8") as fh:
+        for line in fh:
+            match = pattern.match(line)
+
+            if match:
+                return match.group("value")
+        else:
+            raise RuntimeError("Unable to find variable: " + name)
+
+
+__version__ = get_package_variable("__version__")
+
+
+with open("README.md", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="UTF-8") as fh:
+    install_requires = fh.read()
+
+
+setuptools.setup(
+    name="slgero",
+    version="0.0.1",
+    author="Savvov Sergey",
+    author_email="sersavvov@yandex.ru",
+    description="Allow receipt parsing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/slgero/receipt_parser",
+    packages=["receipt_parser"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Natural Language :: Russian",
+    ],
+    install_requires=install_requires,
+    python_requires=">=3.6",
+    keywords=["receipt parser", "product parser", "nlp"],
+)
