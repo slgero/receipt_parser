344ff2409e0872ee8505418b6b5d7a085f8e2f35
slgero <sersavvov@yandex.ru>
2020-08-08 10:49:30 +0300

    Refactoring

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
+"""Setup file."""
 import os.path
 import re
 import setuptools
 
 
-def get_package_variable(name, rel_path="receipt_parser/__init__.py"):
+def get_package_variable(name: str, rel_path: str = "receipt_parser/__init__.py"):
+    """Read pakage variable from `__init__.py`"""
+
     path = os.path.join(os.path.abspath(os.path.dirname(name)), rel_path)
 
     pattern = re.compile(r'^{}.*?([\'"])(?P<value>.+)\1.*$'.format(re.escape(name)))
 
-    with open(path, "r", encoding="UTF-8") as fh:
-        for line in fh:
+    with open(path, "r", encoding="UTF-8") as file:
+        for line in file:
             match = pattern.match(line)
 
             if match:
@@ -20,11 +23,11 @@ def get_package_variable(name, rel_path="receipt_parser/__init__.py"):
 __version__ = get_package_variable("__version__")
 
 
-with open("README.md", "r", encoding="UTF-8") as fh:
-    long_description = fh.read()
+with open("README.md", "r", encoding="UTF-8") as readme:
+    LONG_DESCRIPTION = readme.read()
 
-with open("requirements.txt", "r", encoding="UTF-8") as fh:
-    install_requires = fh.read()
+with open("requirements.txt", "r", encoding="UTF-8") as requires:
+    INSTALL_REQUIRES = requires.read()
 
 
 setuptools.setup(
@@ -33,7 +36,7 @@ setuptools.setup(
     author="Savvov Sergey",
     author_email="sersavvov@yandex.ru",
     description="Allow receipt parsing",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/slgero/receipt_parser",
     packages=setuptools.find_packages(include=["receipt_parser", "receipt_parser.*"]),
@@ -45,7 +48,7 @@ setuptools.setup(
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Natural Language :: Russian",
     ],
-    install_requires=install_requires,
+    install_requires=INSTALL_REQUIRES,
     python_requires=">=3.6",
     keywords=["receipt parser", "product parser", "nlp"],
 )
